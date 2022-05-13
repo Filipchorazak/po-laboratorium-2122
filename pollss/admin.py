9a60d99e397804317ab9c957deed9c8db1759b21
Your Name <you@example.com>
2022-05-13 09:29:33 +0200

    first commit

---
+++ pollss/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Sklepik
+# Register your models here.
+
+
+admin.site.register(Sklepik)
+
---
+++ pollss/models.py
@@ -0,0 +1,26 @@
+from operator import truediv
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+class Sklepik(models.Model):
+    nazwa = models.CharField(max_length=100)
+    opis = models.TextField(blank=True)
+    cena = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self):
+        return self.nazwa
+
+
+
+
+
